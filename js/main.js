59588f60ad490fa7fe51bfb24a0742cc004ea61a
Maddy Ford <mrhf008@gmail.com>
2020-04-19 22:22:56 -0700

    innerHTML method

--- js/main.js
+++ js/main.js
@@ -1,64 +1,66 @@
 const countries = ['United States of America', 'Spain', 'China', 'Brazil', 'Australia'];
 
-fetch("https://api.covid19api.com/summary")
+doFetch = () => {
+    fetch("https://api.covid19api.com/summary")
     .then(response => response.json())
     .then(data => {
-        for (i in data.Countries) {
-            // let countrySelector = document.querySelector("#countries");
-            // countryChoice = document.createElement("option");
-            // countryValue = document.createTextNode(data.Countries[i].Country);
-            // countryChoice.value=data.Countries[i].Country;
-            // countryChoice.id=i;
-            // countryChoice.appendChild(countryValue);
-            // countrySelector.appendChild(countryChoice);
-            if (countries.includes(data.Countries[i].Country)) {
-                console.log(data.Countries[i]);
-                console.log(data.Countries[i].Country);
+        for (country in data.Countries) {
+            if (countries.includes(data.Countries[country].Country)) {
                 let chart = document.querySelector(".BarChart");
-                let height = Math.floor((data.Countries[i].TotalConfirmed) / 1000);
-                let bar = document.createElement("div");
-                let barText = document.createTextNode(data.Countries[i].CountryCode);
-                let barRemove = document.createElement('button');
-                barRemove.innerHTML = 'X';
-                barRemove.onclick=removeCountry;
-                barRemove.classList.add('BarChart-bar-remove');
-                bar.appendChild(barText);
-                bar.appendChild(barRemove);
-                bar.id=i;
-                bar.classList.add("BarChart-bar");
-                bar.style.height = height + "%";
-                chart.appendChild(bar);
+                let height = Math.floor((data.Countries[country].TotalConfirmed) / 1000);
+                chart.innerHTML += `
+                    <div id="bar-${country}" class="BarChart-bar" style="height: ${height}%">
+                        ${data.Countries[country].CountryCode}
+                        <button class="BarChart-bar-remove" onclick=removeCountry(event) >X</button>
+                    </div>
+                `;
             }
         }
-});
+        generateOptions(data);
+    });
+}
+
 
 removeCountry = (event) => {
-    let country = document.getElementById(event.target.parentElement.id );
-    country.remove();
+    let country = document.getElementById(event.target.parentElement.id);
+    let chart = document.querySelector(".BarChart");
+    chart.removeChild(country);
 }
 
 addCountry = () => {
     let newCountry = event.target.value;
+    console.log(countries.length);
     fetch('https://api.covid19api.com/summary')
     .then(response => response.json())
     .then(data => {
         for (country in data.Countries) {
-            if (newCountry === data.Countries[country].Country && !countries.includes(newCountry)) {
-                let chart = document.querySelector(".BarChart");
-                let height = Math.floor((data.Countries[country].TotalConfirmed) / 1000);
-                let bar = document.createElement("div");
-                let barText = document.createTextNode(data.Countries[country].CountryCode);
-                let barRemove = document.createElement('button');
-                barRemove.innerHTML = 'X';
-                barRemove.onclick=removeCountry;
-                barRemove.classList.add('BarChart-bar-remove');
-                bar.appendChild(barText);
-                bar.appendChild(barRemove);
-                bar.id=country;
-                bar.classList.add("BarChart-bar");
-                bar.style.height = height + "%";
-                chart.appendChild(bar);
+            if (newCountry === data.Countries[country].Country &&
+                !countries.includes(newCountry) &&
+                countries.length < 10) {
+                    let chart = document.querySelector(".BarChart");
+                    let height = Math.floor((data.Countries[country].TotalConfirmed) / 1000);
+                    chart.innerHTML += `
+                        <div id="bar-${country}" class="BarChart-bar" style="height: ${height}%">
+                            ${data.Countries[country].CountryCode}
+                            <button class="BarChart-bar-remove" onclick=removeCountry(event) >X</button>
+                        </div>
+                    `;
+                countries.push(data.Countries[country].Country);
             } 
         }
     });
 }
+
+generateOptions = (data) => {
+    for (country in data.Countries) {
+        let countrySelector = document.querySelector("#countries");
+        let countryChoice = document.createElement("option");
+        let countryValue = document.createTextNode(data.Countries[country].Country);
+        countryChoice.value=data.Countries[country].Country;
+        countryChoice.id="option=" + country;
+        countryChoice.appendChild(countryValue);
+        countrySelector.appendChild(countryChoice);
+    }
+}
+
+doFetch();
