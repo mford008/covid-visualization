73b403297dc168ea59d8b2c5a9a45c0e7cb489de
Maddy Ford <mrhf008@gmail.com>
2020-05-25 12:05:45 -0700

    WIP for removing country, adding HTML for global data

--- covid-react/src/App.js
+++ covid-react/src/App.js
@@ -5,13 +5,20 @@ let countries = ['United States of America', 'Spain', 'China', 'Brazil', 'Austra
 
 class App extends Component {
   state = {
+    globalData: [],
     data: [],
     displayCountries: [],
+    selectedCountry: '',
+    selectedCountryTotal: '',
+    selectedCountryDeaths: '',
+    selectedCountryRecovered: '',
   }
   componentDidMount() {
     fetch("https://api.covid19api.com/summary")
       .then(response => response.json())
       .then(data =>  {
+        console.log(data);
+        this.setState({ globalData: data.Global })
         this.setState({ data: data.Countries })
         this.setState({ displayCountries: (this.state.data).filter(datum => (countries.includes(datum.Country)))})
       })
@@ -21,11 +28,37 @@ class App extends Component {
     this.state.data.map(each => {
       if (country === each.Country) {
         this.setState({
-          displayCountries: this.state.displayCountries.concat(each)
+          displayCountries: this.state.displayCountries.concat(each),
+          selectedCountry: each.Country,
+          selectedCountryTotal: each.TotalConfirmed,
+          selectedCountryDeaths: each.TotalDeaths,
+          selectedCountryRecovered: each.TotalRecovered,
         });
-        console.log(this.state.displayCountries);
       }
     }) 
+  }
+
+  removeCountry(country) {
+    console.log(country);
+    // this.state.data.map(each => {
+    //   if (country === each.Country) {
+    //     this.setState({
+    //       displayCountries: this.state.displayCountries.splice(each)
+    //     });
+    //     console.log(this.state.displayCountries);
+    //   }
+    // }) 
+  }
+
+  displayStats(abbreviation) {
+    console.log(this.state.displayCountries);
+    const selected = this.state.displayCountries.filter(country => country.CountryCode === abbreviation);
+    this.setState({
+      selectedCountry: selected[0].Country,
+      selectedCountryTotal: selected[0].TotalConfirmed,
+      selectedCountryDeaths: selected[0].TotalDeaths,
+      selectedCountryRecovered: selected[0].TotalRecovered,
+    })
   }
 
   render() {
@@ -35,21 +68,30 @@ class App extends Component {
         <div className="Header">
           <h1 className="Header-content">Data Visualization for COVID-19</h1>
         </div>
-        <div className="Selector">
-            <label htmlFor="countries">Select Country:</label>
-            <select id="countries" onChange={event => this.addCountry(event.target.value)}>
-            {(this.state.data).map(datum => (
-              <option>
-                {datum.Country}
-              </option>
-            ))
-          }
-            </select>
+        <div className="Subheader">
+          <div className="Selector">
+              <label className="Selector-label" htmlFor="countries">Select Country: </label>
+              <select id="countries" onChange={(event) => this.addCountry(event.target.value)}>
+              {(this.state.data).map(datum => (
+                <option>
+                  {datum.Country}
+                </option>
+              ))
+            }
+              </select>
+          </div>
+          <div className="DisplayStats">
+          <p className="DisplayStats-content">Country: <span className="DisplayStats-content-data">{this.state.selectedCountry}</span></p>
+          <p className="DisplayStats-content">Confirmed Cases: <span className="DisplayStats-content-data">{this.state.selectedCountryTotal}</span></p>
+          <p className="DisplayStats-content">Deaths: <span className="DisplayStats-content-data">{this.state.selectedCountryDeaths}</span></p>
+          <p className="DisplayStats-content">Recoveries: <span className="DisplayStats-content-data">{this.state.selectedCountryRecovered}</span></p>
+          </div>
         </div>
         <div className="BarChart">
           {(this.state.displayCountries).map(datum => (
-            <div className="BarChart-bar" style={{height: (datum.TotalConfirmed / 1000) + "%"}}>
-            {datum.CountryCode}
+            <div className="BarChart-bar" onClick={(event) => this.displayStats(event.target.innerText)} style={{height: (datum.TotalConfirmed / 1000) + "%"}}>
+            {datum.CountryCode} 
+            {/* <button onClick={(event) => this.removeCountry(event)}>X</button> */}
             </div>
             ))
           }
